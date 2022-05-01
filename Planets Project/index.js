24651f6e32a9461857c2cd0bcf7a0b35f55115d0
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-05-01 16:22:40 +0100

    read the data and parsed it using streams

--- Planets Project/index.js
+++ Planets Project/index.js
@@ -1,1 +1,22 @@
-const parse = require('csv-parse');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', (data) => {
+    results.push(data);
+  })
+  .on('error', (err) => {
+    console.log('(IOB) Error at reading the file: ', err);
+  })
+  .on('end', () => {
+    console.log(results);
+    console.log('done reading the file');
+  });
