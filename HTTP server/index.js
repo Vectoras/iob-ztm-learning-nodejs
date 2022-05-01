291419cc212ccc96ce69f5121af2b078b7322c20
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-05-01 19:56:59 +0100

    simple endpoints

--- HTTP server/index.js
+++ HTTP server/index.js
@@ -1,1 +1,67 @@
 const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer();
+
+const friends = [
+  {
+    id: 0,
+    name: 'Nicola Tesla',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Albert Einstein',
+  },
+];
+
+server.on('request', (req, res) => {
+  if (req.url === '/json') {
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+
+    res.end(
+      JSON.stringify({
+        type: 'json',
+        messge: 'Hello! Sir Isaac Newton is your friend!',
+      })
+    );
+  } else if (req.url === '/html') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+
+    // res.statusCode(200);
+    // res.setHeader('Content-Type', 'text/html');
+
+    res.write('<html>');
+    res.write('<body>');
+    res.write('<ul>');
+    res.write('<li> Hellow Isaac!</li>');
+    res.write('<li>What are your thoughts on astronomy?</li>');
+    res.write('</ul>');
+    res.write('</body>');
+    res.write('</html>');
+
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(
+    '\nListening on port ' +
+      PORT +
+      '... \naddress: ' +
+      '\033[1;34mhttp://localhost:' +
+      PORT +
+      '\033[0m'
+  );
+});
