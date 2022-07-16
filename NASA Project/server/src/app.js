56b5f0738d1ce4390befde73cbd3a31641df57a4
Ionut Boghean <johnny.boghean.developer@gmail.com>
2022-07-16 10:22:23 +0100

    corrected automation for multiple folders

--- NASA Project/server/src/app.js
+++ NASA Project/server/src/app.js
@@ -1,6 +1,9 @@
 // importing npm modules
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
+console.log(__dirname);
 
 // creating the app
 const app = express();
@@ -9,14 +12,12 @@ const app = express();
 const planetsRouter = require('./routes/planets/planets.router');
 
 // middleware
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-  })
-);
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // using routes
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '..', 'public', 'index.html')));
 app.use(planetsRouter);
 
 // exports
